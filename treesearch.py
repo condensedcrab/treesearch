fa23ceb75c33c8e2bb47284c2f12bfebc6bed3ef
david <david>
2025-01-02 10:07:31 -0800

    tracking session_token as a class variable

--- treesearch.py
+++ treesearch.py
@@ -11,36 +11,30 @@ class SatImg():
     
     def __init__(self):
         self.data = []
+        self.session_token = ""
 
 
-def get_session_url(api_key):
-    create_session_url = "https://tile.googleapis.com/v1/createSession"
+    def get_session_url(self):
+        create_session_url = "https://tile.googleapis.com/v1/createSession"
 
-    payload = {
-        "mapType": "satellite",
-        "language": "en-US",
-        "region": "US",
-    }
+        payload = {
+            "mapType": "satellite",
+            "language": "en-US",
+            "region": "US",
+        }
 
-    headers = {"Content-Type": "application/json"}
+        headers = {"Content-Type": "application/json"}
 
-    response = requests.post(
-        create_session_url, json=payload, headers=headers, params={"key": api_key}
-    )
+        response = requests.post(
+            create_session_url, json=payload, headers=headers, params={"key": MY_API}
+        )
 
-    if response.status_code == 200:
-        session_token = response.json().get("session")
-        print("Session token:", session_token)
-    else:
-        print("Failed to create session:", response.text)
+        if response.status_code == 200:
+            self.sesion_token = response.json().get("session")
+            print("Session token:", session_token)
+        else:
+            print("Failed to create session:", response.text)
 
-    return (
-        "https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session="
-        + session_token
-        + "&key="
-        + api_key
-    )
-    
 def get_tile()
 
 
