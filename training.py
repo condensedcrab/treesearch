2f4abeed0cc0bd6f56acd15149d81cf66c1bcd0c
David <david.barbalas@gmail.com>
2025-01-06 20:12:45 -0800

    trying to use yolo in python instead of CLI to make sure I have environment variables configured correctly.

--- training.py
+++ training.py
@@ -12,7 +12,7 @@ import os
 load_dotenv()
 ultralytics.checks()
 
-# %%
+# %% setup roboflow data (if not already present)
 
 
 ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API") # add Roboflow api to .env file in main git repo
@@ -23,3 +23,25 @@ project = workspace.project("palms-nfvyz")
 version = project.version(3)
 dataset = version.download("yolov11")
 # %%
+# !yolo train model=yolo11n.pt data=palms-3/data.yml epochs=3 imgsz=640
+
+# %% run YOLO model in Python
+from ultralytics import YOLO
+
+# Create a new YOLO model from scratch
+model = YOLO("yolo11n.yaml")
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO("yolo11n.pt")
+
+# Train the model using the 'coco8.yaml' dataset for 3 epochs
+results = model.train(data="coco8.yaml", epochs=3)
+
+# Evaluate the model's performance on the validation set
+results = model.val()
+
+# Perform object detection on an image using the model
+results = model("https://ultralytics.com/images/bus.jpg")
+
+# Export the model to ONNX format
+success = model.export(format="onnx")
