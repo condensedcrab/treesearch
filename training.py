b8073be085591f1e9110b243c1afc25ccbd04190
David <david.barbalas@gmail.com>
2025-01-06 21:56:50 -0800

    playing around with the yolo training parameters

--- training.py
+++ training.py
@@ -21,19 +21,22 @@ project = workspace.project("palms-nfvyz")
 version = project.version(3)
 dataset = version.download("yolov11")
 # %%
-# !yolo train model=yolo11n.pt data=palms-3/data.yml epochs=3 imgsz=640
+# !yolo train model=yolo11n.pt data=/home/david/git/treesearch/palms-3/data.yml epochs=3 imgsz=640
 
 # %% run YOLO model in Python
 from ultralytics import YOLO
 
-# Create a new YOLO model from scratch
-model = YOLO("yolo11n.yaml")
+# # Create a new YOLO model from scratch
+# model = YOLO("yolo11n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO("yolo11n.pt")
+# model = YOLO("/home/david/git/yolo11/runs/detect/train15_200/weights/last.pt")  # load a partially trained model
 
 # Train the model using data from Roboflow 
-results = model.train(data=f"{dataset.location}/data.yaml", epochs=200)
-
+results = model.train(data=f"{dataset.location}/data.yaml", epochs=5000,name="palms_v3",patience=200)
+# results = model.train(data=f"{dataset.location}/data.yaml", epochs=5000, resume=True,patience=100)
 # Evaluate the model's performance on the validation set
 results = model.val()
+
+
