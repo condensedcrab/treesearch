289613d0bc690fa154f1f55f9a866e1750335b02
David <david.barbalas@gmail.com>
2025-01-07 12:30:31 -0800

    removing nano model and using the medium for better accuracy.

--- training.py
+++ training.py
@@ -7,6 +7,7 @@ from PIL import Image
 from roboflow import Roboflow
 from dotenv import load_dotenv
 import os
+from ultralytics import YOLO
 
 # %load_ext tensorboard
 # %tensorboard --logdir /home/david/git/yolo11/runs
@@ -26,21 +27,40 @@ dataset = version.download("yolov11")
 # !yolo train model=yolo11n.pt data=/home/david/git/treesearch/palms-3/data.yml epochs=3 imgsz=640
 
 # %% run YOLO model in Python
-from ultralytics import YOLO
+
 
 # # Create a new YOLO model from scratch
 # model = YOLO("yolo11n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
-# model = YOLO("yolo11n.pt")
+model = YOLO("yolo11m.pt") # use medium model
 model = YOLO("/home/david/git/yolo11/runs/detect/palms_v3/weights/last.pt")  # load a partially trained model
 
 # Train the model using data from Roboflow 
-results = model.train(data=f"{dataset.location}/data.yaml", epochs=500,resume=True,name="palms_v3_half",patience=100)
+results = model.train(data=f"{dataset.location}/data.yaml", epochs=500,resume=True,name="palms_v3",patience=100)
 # results = model.train(data=f"{dataset.location}/data.yaml", epochs=5000, resume=True,patience=100)
 # Evaluate the model's performance on the validation set
 results = model.val()
 
 # %% 
+# using ultralytics docs: https://docs.ultralytics.com/modes/predict/#__tabbed_1_1
+import cv2
+import supervision as sv
+import glob
 
+model = YOLO("/home/david/git/yolo11/runs/detect/palms_v3/weights/last.pt")  # load a partially trained model
 
+img_files = glob.glob("/home/david/git/treesearch/data/thousand_palms_640x640_z20_50x50/*.png")
+
+# Run batched inference on a list of images
+results = model(img_files[0:5],stream=True)  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename="result.png")  # save to disk
