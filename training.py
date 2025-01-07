8566f6dccce513976690fac638a4e11adf48ccf1
David <david.barbalas@gmail.com>
2025-01-06 19:54:04 -0800

    adding google colab notebook for refernce

--- training.py
+++ training.py
@@ -1,4 +1,8 @@
 # following instructions from: https://blog.roboflow.com/yolov11-how-to-train-custom-data/
+# and Google colab notebook: https://colab.research.google.com/github/roboflow-ai/notebooks/blob/main/notebooks/train-yolo11-object-detection-on-custom-dataset.ipynb?ref=blog.roboflow.com#scrollTo=1nOnTQynZfeA
 # 
 import ultralytics
+from PIL import Image
+
+
 ultralytics.checks()
