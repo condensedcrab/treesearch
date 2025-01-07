ac90efc13cb62c5be60f377724c0d5b2248cd028
David <david.barbalas@gmail.com>
2025-01-06 19:58:17 -0800

    updating pip install list

--- training.py
+++ training.py
@@ -1,8 +1,23 @@
+# %%
 # following instructions from: https://blog.roboflow.com/yolov11-how-to-train-custom-data/
 # and Google colab notebook: https://colab.research.google.com/github/roboflow-ai/notebooks/blob/main/notebooks/train-yolo11-object-detection-on-custom-dataset.ipynb?ref=blog.roboflow.com#scrollTo=1nOnTQynZfeA
-# 
+
 import ultralytics
 from PIL import Image
+from roboflow import Roboflow
+from dotenv import load_dotenv
 
 
+load_dotenv()
 ultralytics.checks()
+
+# %%
+
+
+ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API") # add Roboflow api to .env file in main git repo
+rf = Roboflow(api_key=ROBOFLOW_API_KEY)
+
+workspace = rf.workspace("liangdianzhong")
+project = workspace.project("-qvdww")
+version = project.version(3)
+dataset = version.download("yolov11")
