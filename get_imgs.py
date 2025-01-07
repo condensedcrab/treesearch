b65034c45552b14e6639e62c23d653a6afd79d24
David <david.barbalas@gmail.com>
2025-01-07 12:12:49 -0800

    adding new grid of images with the script

--- get_imgs.py
+++ get_imgs.py
@@ -1,7 +1,9 @@
-
 # %%
-s = SatImg()
+import SatImg
+
+s = SatImg.SatImg()
 
 # use coordinate spacing to construct grid
 # s.get_static_grid(33.821179, -116.394663, 100, 100) # old version
 s.get_static_grid(33.832183, -116.534736,25,25)
+# %%
