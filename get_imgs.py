229b2fe39b3b7f4f2d1368488f5e9cc3ebe186be
David <david.barbalas@gmail.com>
2025-01-16 23:47:32 -0800

    get_imgs is setup and ready to go.

--- get_imgs.py
+++ get_imgs.py
@@ -4,5 +4,5 @@ import SatImg as si
 s = si.SatImg()
 
 # use coordinate spacing to construct grid. GMaps only goes out to 6 decimal places
-s.get_static_grid("data/thousand_palms_640x640_z20_50x50",33.832183, -116.534736,1,1)
+s.get_static_grid(33.832183, -116.534736,4,4,file_path="data/thousand_palms_640x640_z20_50x50")
 
