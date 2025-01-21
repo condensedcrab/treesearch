b1f778bad7b370df7a9a199614e8c93e9f827edf
David <david.barbalas@gmail.com>
2025-01-20 20:54:33 -0800

    adding confidence levels as a list instead of singular value

--- analysis.py
+++ analysis.py
@@ -25,29 +25,30 @@ model = YOLO(
 results = model.val()
 
 # %% set up formatting on the df
-conf_limit = 0.25  # >= this value only
+conf_limit = [0.1, 0.25, 0.5]  # >= this value only
 
-rows = df["confidence"] >= conf_limit
-df = df[rows]
-
-
-num_palms = 0
-
-for index, row in df.iterrows():
-    img_filename = row["file_name"]
-    coords = re.findall(r"-?\d+\.\d+", img_filename)
-    coords = [float(coords[0]), float(coords[1])]
-
-    flag_valid_lat = coords[0] >= np.min([output[0][0], output[1][0]]) and coords[
-        0
-    ] <= np.max([output[0][0], output[1][0]])
-
-    flag_valid_long = coords[1] >= np.min([output[0][1], output[1][1]]) and coords[
-        1
-    ] <= np.max([output[0][1], output[1][1]])
-
-    if flag_valid_lat and flag_valid_long:
-        num_palms += 1
+for conf_lvl in conf_limit:
+    rows = df["confidence"] >= conf_lvl
+    df = df[rows]
 
 
-print(f"Valid palms found: {num_palms}")
+    num_palms = 0
+
+    for index, row in df.iterrows():
+        img_filename = row["file_name"]
+        coords = re.findall(r"-?\d+\.\d+", img_filename)
+        coords = [float(coords[0]), float(coords[1])]
+
+        flag_valid_lat = coords[0] >= np.min([output[0][0], output[1][0]]) and coords[
+            0
+        ] <= np.max([output[0][0], output[1][0]])
+
+        flag_valid_long = coords[1] >= np.min([output[0][1], output[1][1]]) and coords[
+            1
+        ] <= np.max([output[0][1], output[1][1]])
+
+        if flag_valid_lat and flag_valid_long:
+            num_palms += 1
+
+
+    print(f"At conf level {conf_lvl}, palms found: {num_palms}")
